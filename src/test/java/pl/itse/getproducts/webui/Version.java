acefa326f3c3505036fea4705f69c24fbda32f4e
bpawluczuk <bpawluczuk@onet.pl>
2017-05-16 21:47:15 +0200

    version

---
+++ src/test/java/pl/itse/getproducts/webui/Version.java
@@ -0,0 +1,8 @@
+package pl.itse.erp.warehouse.application.webui;
+
+/**
+ * Created by Borys on 2017-05-08.
+ */
+public class Version {
+    public static final String WAREHOUSE_V1 = "application/vnd.pl.itse.erp.warehouse.v1+json";
+}
